dcc876d611d084b5cf4d206b6d36fa69020ad20d
martin champarini <martin.champarini.dev@gmail.com>
2022-03-06 21:00:42 -0300

    added cucumber

---
+++ src/test/java/cucumber/CucumberSpringConfiguration.java
@@ -0,0 +1,15 @@
+package cucumber;
+
+
+import com.example.democucumber.DemoCucumberApplication;
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.test.context.SpringBootContextLoader;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@CucumberContextConfiguration
+@SpringBootTest(classes = DemoCucumberApplication.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@ContextConfiguration(classes = DemoCucumberApplication.class, loader = SpringBootContextLoader.class)
+public class CucumberSpringConfiguration {
+
+}
